1aba70d63cf831653eec54680419b9700eec3175
uomma <marcelloleone975@gmail.com>
2023-03-14 16:24:17 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+ const { createApp } = Vue;
+
+const app = createApp({
+    data(){
+        return {
+            testo:'il pollo crudo non fa bene'
+        }
+    }
+  
+}).mount('#app');
